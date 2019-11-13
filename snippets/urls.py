f6db24149dddc6673dd6c8c407bd765df359efd7
pejvak61 <a.mohammadi.t@live.com>
2019-11-14 00:11:25 +0330

    some changes

--- snippets/urls.py
+++ snippets/urls.py
@@ -8,7 +8,7 @@ from snippets import views
 
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/', views.SnippetList.as_view(),name='snippets_Ali'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
