7f33e7d87133172ecdecf51dbe4698accee7b8f3
pejvak61 <a.mohammadi.t@live.com>
2019-11-16 00:07:20 +0330

    changes

--- useraccounts/models.py
+++ useraccounts/models.py
@@ -17,12 +17,14 @@ class TblUserAccounts(models.Model):
 class TblUserDetails(models.Model):
     detail_id = models.IntegerField(primary_key=True)
     useraccount = models.IntegerField()
+    # useraccount = models.ForeignKey(TblUserAccounts, on_delete=models.DO_NOTHING, related_name='TblUserAccounts.uid', db_column='useraccount')
     # useraccount = models.ForeignKey(TblUserAccounts, models.DO_NOTHING, related_name='TblUserAccounts.uid')
     first_name = models.CharField(max_length=25, blank=True, null=True)
     last_name = models.CharField(max_length=45, blank=True, null=True)
     birthdate = models.DateTimeField(blank=True, null=True)
     record_time = models.DateTimeField()
     creator = models.IntegerField()
+    # creator = models.ForeignKey(TblUserAccounts, on_delete=models.DO_NOTHING, related_name='TblUserAccounts.uid', db_column='creator')
     # creator = models.ForeignKey(TblUserAccounts, models.DO_NOTHING, related_name='TblUserAccounts.uid', db_column='creator')
 
     class Meta:
@@ -34,11 +36,13 @@ class TblUserDetails(models.Model):
 class TblUserPassword(models.Model):
     id_password = models.AutoField(primary_key=True)
     useraccount_id_pwd = models.IntegerField()
+    # useraccount_id_pwd = models.ForeignKey(TblUserAccounts, on_delete=models.DO_NOTHING, related_name='TblUserAccounts.uid', db_column='useraccount_id_pwd')
     # useraccount_id_pwd = models.ForeignKey(TblUserAccounts, models.DO_NOTHING, related_name='TblUserAccounts.uid', db_column='useraccount_id_pwd')
     salt = models.CharField(max_length=200, blank=True, null=True)
     hash = models.CharField(max_length=200, blank=True, null=True)
     record_time = models.DateTimeField()
     creator = models.IntegerField()
+    # creator = models.ForeignKey(TblUserAccounts, on_delete=models.DO_NOTHING, related_name='TblUserAccounts.uid', db_column='creator')
     # creator = models.ForeignKey(TblUserAccounts, models.DO_NOTHING, related_name='TblUserAccounts.uid', db_column='creator')
 
     class Meta:
